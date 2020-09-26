f1d1464652d1e15ba07899ebba85278e500bb95c
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-09-27 01:05:14 +0300

    добавь стили!!!

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import css from "./css/style.css";
+import posts from './ posts.json';
+console.log(posts);
+import postTemplate from './post.hbs';
+console.log(postTemplate);
+
+const itemTemplate = postTemplate(posts);
+console.log(itemTemplate);
+
+const listItem = document.getElementById('myFavouritePosts');
+console.log(listItem);
+listItem.insertAdjacentHTML('afterbegin', itemTemplate);
